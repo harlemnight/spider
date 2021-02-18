10252201e543b97a663752ad044d9d23be8c5f1b
harlemnight <33836858@qq.com>
2021-02-18 13:23:25 +0800

    123

--- spider_jqdata_swhy.py
+++ spider_jqdata_swhy.py
@@ -3,20 +3,31 @@ import pandas as pd
 
 
 def get_jqdata_swhy(level):
-     rs = get_industries(name=level)
-     print(rs)
-     dao = rs.to_dict(orient='index')
-     print(dao)
-     print(dao.keys())
+     """
+        获取聚宽申万行业代码表
+     :param level: 聚宽申万行业1，2，3级 sw_l1,sw_l2,sw_l3
+     :return:
+     """
+     data = get_industries(name=level)
+     rs = dict(zip(data.index, data['name']))
+     return rs
 
-     print(dao.values()[])
-     #dao2 = dict(zip(dao.keys(),dao.values()['name']))
-     #print(dao2)
 
+def get_jqdata_swhy_stocks(hy_dm):
+    """
+       获取申万行业成份股
+    :param level: 聚宽申万行业1，2，3级 sw_l1,sw_l2,sw_l3
+    :return:
+    """
+    stocks = get_industry_stocks(hy_dm)
+    return stocks
 
 if __name__ == '__main__':
     auth('13908366866', '366866')
-    rs = get_jqdata_swhy('sw_l1')
-
+    rs = get_jqdata_swhy('sw_l3')
+    print(rs)
+    datas = get_jqdata_swhy_stocks('801081')
+    print(datas)
+    print(len(datas))
     #d = get_industry("600519.XSHG", date=None)
     # print(rs)
