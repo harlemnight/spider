c18943b648050e319a4c48872c20f283e9abe3bd
harlemnight <33836858@qq.com>
2021-03-03 16:46:10 +0800

    123

---
+++ spider_hsgt.py
@@ -0,0 +1,110 @@
+import requests
+from requests.exceptions import RequestException
+from urllib.parse import urlencode
+import spider_cons as spcon
+import datetime as dt
+import ast
+from lxml import etree
+import re
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import ChromeOptions
+import time
+import urllib.parse
+import urllib.request
+import urllib.error
+
+
+def get_stock_hsgt_tj(symbol, source):
+    """
+        获取股票沪深股通统计信息
+    Parameters
+    source : eastmoney（东方财富）
+    Return
+    --------
+    """
+    base_url = spcon.HSGT_TJ_STOCK[source]['url']
+    headers = spcon.HSGT_TJ_STOCK[source]['headers']
+    params = spcon.HSGT_TJ_STOCK[source]['params']
+    try:
+        base_url = base_url % symbol
+        print(base_url + urlencode(params))
+        response = requests.get(base_url + urlencode(params), headers=headers)
+        if response.status_code == 200:
+            return parse_hsgttj_data(response, symbol, source)
+    except RequestException:
+        return None
+
+
+def parse_hsgttj_data(response, symbol, source):
+    """
+        解析股票沪深股通统计信息
+    Parameters
+    response : 网络响应
+    source : eastmoney（东方财富）
+    Return
+    --------
+     """
+    hsgts = []
+    # eastmoney（东方财富）
+    if source == 'eastmoney':
+        hsgt = response.json().get('result').get('data')
+        if hsgt:
+            for i in range(len(hsgt)):
+                data = {}
+                data['rq'] = hsgt[i].get('DATE')
+                data['symbol'] = symbol
+                data['symbol_name'] = hsgt[i].get('SECNAME')
+                hsgt.append(data)
+    return hsgts
+
+
+def get_stock_hsgt_mx(symbol, source):
+    """
+        获取股票沪深股通明细信息
+    Parameters
+    source : eastmoney（东方财富）
+    Return
+    --------
+    """
+    base_url = spcon.HSGT_MX_STOCK[source]['url']
+    headers = spcon.HSGT_MX_STOCK[source]['headers']
+    params = spcon.HSGT_MX_STOCK[source]['params']
+    try:
+        base_url = base_url % symbol
+        print(base_url + urlencode(params))
+        response = requests.get(base_url + urlencode(params), headers=headers)
+        if response.status_code == 200:
+            return parse_hsgtmx_data(response, symbol, source)
+    except RequestException:
+        return None
+
+
+def parse_hsgtmx_data(response, symbol, source):
+    """
+        解析股票沪深股通明细信息
+    Parameters
+    response : 网络响应
+    source : eastmoney（东方财富）
+    Return
+    --------
+     """
+    hsgts = []
+    # eastmoney（东方财富）
+    if source == 'eastmoney':
+        hsgt = response.json().get('result').get('data')
+        if hsgt:
+            for i in range(len(hsgt)):
+                data = {}
+                data['rq'] = hsgt[i].get('DATE')
+                data['symbol'] = symbol
+                data['symbol_name'] = hsgt[i].get('SECNAME')
+                hsgt.append(data)
+    return hsgts
+
+
+if __name__ == '__main__':
+    rs = get_stock_hsgt_tj('000070', 'eastmoney')
+    print(rs)
+    rs = get_stock_hsgt_mx('000070', 'eastmoney')
+    print(rs)
